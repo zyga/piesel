2fb735a53d529ae086c9bfb21210397ac6631031
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-11-12 14:47:01 +0100

    Another update, almost ready, wow

--- piesel/setup.py
+++ piesel/setup.py
@@ -2,7 +2,6 @@
 
 from distutils.core import setup
 
-
 setup(
     name="pieseł",
     version="1.0",
@@ -10,6 +9,11 @@ setup(
     url="https://github.com/zyga/piesel",
     scripts=['pieseł'],
     data_files=[
-        ('/usr/share/icons/hicolor/128x128/apps', ['pieseł.png']),
+        ('/usr/share/icons/hicolor/256x256/apps',
+            ['icons/256x256/pieseł.png']),
+        ('/usr/share/icons/hicolor/128x128/apps',
+            ['icons/128x128/pieseł.png']),
+        ('/usr/share/icons/hicolor/64x64/apps',
+            ['icons/64x64/pieseł.png']),
         ('/usr/share/applications', ['pieseł.desktop'])
     ])
